e7475f2b34d459ad03a5d3aea37b23b3bbf61ba0
ahmadi330040 <ahmadi330040@gmail.com>
2021-08-10 10:40:08 +0700

    menambahkan container

--- aplikasi_flutter/lib/main.dart
+++ aplikasi_flutter/lib/main.dart
@@ -1,19 +1,24 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+void main(){
+  runApp(new MaterialApp(
+    home: new HalamanSatu(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text("Aplikasi Hello World"),),
-        body: Text("Hello World"),
-      ),
+class HalamanSatu extends StatelessWidget{
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: 
+      new Center(child: new Container(
+        color: Colors.blue[700],
+        width: 200,
+        height: 100,
+        child: Center(
+          child: new Icon(Icons.android, color: Colors.white, size: 70,),
+        ),
+      ),)
+      
     );
   }
 }
